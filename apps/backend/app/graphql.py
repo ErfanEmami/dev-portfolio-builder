641751dc32351eee0828a05e693b2ae7195922cd
ErfanEmami <erfan.emami123@gmail.com>
2025-02-28 00:47:52 -0700

    backend .gitignore

--- apps/backend/app/graphql.py
+++
@@ -1,6 +0,0 @@
-import strawberry
-from strawberry.fastapi import GraphQLRouter
-from app.resolvers import Query
-
-schema = strawberry.Schema(query=Query, mutation=Mutation)
-graphql_app = GraphQLRouter(schema)
